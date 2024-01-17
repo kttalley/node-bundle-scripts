d179303ad6fbd5bd517375c6d06bf23191305c04
Kristian <talleykristian@gmail.com>
2024-01-16 20:58:47 -0800

    using a regular expression to remove head tag from output to prevent missing resource errors

--- vanilla-js-bundler.js
+++ vanilla-js-bundler.js
@@ -6,8 +6,11 @@ const html = fs.readFileSync('src/index.html', 'utf8');
 const css = fs.readFileSync('src/styles.css', 'utf-8');
 const js = fs.readFileSync('src/scripts.js', 'utf-8');
 
-//combine files into a single HTML string
-const bundledHTML = `<html><head><style>${css}</style><script>${js}</script></head><body>${html}</body></html>`;
+// Extract content inside the body tag
+const bodyContent = /<body>([\s\S]+)<\/body>/i.exec(html)[1];
+
+// Combine files into a single HTML string (excluding head tag)
+const bundledHTML = `<html><head><style>${css}</style><script>${js}</script></head><body>${bodyContent}</body></html>`;
 
 //minify the combined HTML
 const minifiedHTML = minify(bundledHTML, {
